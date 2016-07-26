76d010a20005e61857409fdb41fe162038e2ab30
LuisaDavila <davilalu28@gmail.com>
2016-07-26 00:05:12 -0700

    Delete script(2).js

--- Assignment6/js/script(2).js
+++
@@ -1,7 +0,0 @@
-function checkedOff () {
-    if (document.getElementById("handTossed").checked) {
-        document.getElementById("ifHandTossed").style.visibility= "visible";
-    } else document.getElementById("ifHandTossed").style.visibility= "hidden";
-}
-
-//get setIndexOf ???
